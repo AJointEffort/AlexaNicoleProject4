df22fbada6df9c26fcc4d58c86b375f8fcf55d9c
Nicole Lavergne <nicole.s.lavergne@gmail.com>
2019-08-31 13:18:31 -0400

    trying at add next question

--- script/script.js
+++ script/script.js
@@ -4,6 +4,7 @@ const weedApp = {};
 // CREATE VARIABLES
 weedApp.init = function(){
     weedApp.submitClick();
+    weedApp.moodSubmitClick();
 }
 
 // CREATE KEYS FOR URL AND APIKEY
@@ -12,15 +13,17 @@ weedApp.apiKey = 'u3jgDMY';
 // CREATE FUNCTION FOR AJAX CALL
 const strainArray = weedApp.getStrain = (userInput) => {
     $.ajax({
-        url: `http://strainapi.evanbusse.com/${weedApp.apiKey}/strains/search/race/${userInput}` ,
+        url: `https://strainapi.evanbusse.com/${weedApp.apiKey}/strains/search/effect/${userInput}` ,
         method: 'GET',
+        
         datatype: 'json'
     }).then((res) => {
         // CALL RANDOM STRAIN FUNCTION
         let singleStrain = (res[Math.floor(Math.random() * res.length)]);
-        console.log("printing the strain object",singleStrain);
-        console.log(singleStrain.name);
-        console.log(singleStrain.race);
+        // console.log("printing the strain object",singleStrain);
+        console.log(res);
+        // console.log(singleStrain.race);
+
         // CALL RESULTS FUNCTION
         $(".userResultsContainer").append(`<h2 class="userResultTitle">Try this!</h2>`)
         $(".userResultsContainer").append(`<img src="images/${userInput}.jpg">`);
@@ -29,24 +32,38 @@ const strainArray = weedApp.getStrain = (userInput) => {
        
     } 
 )};
+// create a function for first submit button to bring you to next question
 
+weedApp.moodSubmitClick = function() {
+    $('.moodForm').submit(function (event){
+        event.preventDefault();
+        console.log("clicking the mood form submit");
+
+        if($("input[name=strain]:checked").length){
+            console.log("clicking strain submit");
+            
+            showActivityIndica();
+        }
+    });
+}
 // CREATE FUNCTION FOR ON SUBMIT BY USER
 weedApp.submitClick = function () {
-    $('form').submit(function (event) {
+    $('.activityForm').submit(function (event) {
         event.preventDefault();
         console.log('clicked!');
 
-        const strain = $("input[name=strain]:checked").val();
-        
+        const strain = $("input[name=activity]:checked").val();
+        // user input from mood question = true or matches a strain 
+
         animatedResults();
 
         $('html, body').animate({
-            scrollTop: $('.formContainer').offset().top + $('.formContainer').height()
+            scrollTop: $('.moodFormContainer').offset().top + $('.moodFormContainer').height()
         }, 400);
 
-        if ($("input[name=strain]:checked").length) {
+        if ($("input[name=activity]:checked").length) {
             $('input[type=submit]', this).attr('disabled', 'disabled');
-        } else if (!$("input[name=strain]:checked").length) {
+        } else if (!$("input[name=activity]:checked").length) {
             alert(`Are you already high? Must choose one option before submitting!`);
         };
 
@@ -55,6 +72,9 @@ weedApp.submitClick = function () {
     });
 };
 
+showActivityChoices = function(){
+    $('.activityIndica').css('opcacity', '1').css('visibility', 'visible');
+}
 animatedResults = function(){
     $('.resultContainer').css('opacity', '1').css('visibility', 'visible');
 }
