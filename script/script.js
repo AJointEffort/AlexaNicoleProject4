5a332946a66afac9fefee20bac5f8dd28e30e271
Alexa <alexawoloszuk@gmail.com>
2019-09-02 16:32:30 -0400

    final commit

--- script/script.js
+++ script/script.js
@@ -2,61 +2,58 @@
 const weedApp = {};
 
 // CREATE VARIABLES
-weedApp.init = function(){
+weedApp.init = function(){ 
     weedApp.submitClick();
 }
 
-// CREATE KEYS FOR URL AND APIKEY
+// CREATE KEYS FOR URL AND APIKEY 
 weedApp.apiKey = 'u3jgDMY';
 
 // CREATE FUNCTION FOR AJAX CALL
-const strainArray = weedApp.getStrain = (userInput) => {
+const raceArray = weedApp.getRace = (userInput) => {
     $.ajax({
-        url: `http://strainapi.evanbusse.com/${weedApp.apiKey}/strains/search/race/${userInput}` ,
+        url: `https://strainapi.evanbusse.com/${weedApp.apiKey}/strains/search/race/${userInput}` ,
         method: 'GET',
         datatype: 'json'
     }).then((res) => {
-        // CALL RANDOM STRAIN FUNCTION
-        let singleStrain = (res[Math.floor(Math.random() * res.length)]);
-        console.log("printing the strain object",singleStrain);
-        console.log(singleStrain.name);
-        console.log(singleStrain.race);
-        // CALL RESULTS FUNCTION
+        // CALLING RANDOM STRAIN FUNCTION
+        let singleRace = (res[Math.floor(Math.random() * res.length)]);
+
+        // CALLING RESULTS FUNCTION
         $(".userResultsContainer").append(`<h2 class="userResultTitle">Try this!</h2>`)
         $(".userResultsContainer").append(`<img src="images/${userInput}.jpg">`);
-        $(".userResultsContainer").append(`<p class="apiName"><span class="resultSpan">Strain:</span> ${singleStrain.name}</p>`);
-        $(".userResultsContainer").append(`<p class="apiRace"><span class="resultSpan">Race: </span> ${singleStrain.race}</p>`);
+        $(".userResultsContainer").append(`<p class="apiName"><span class="resultSpan">Strain:</span> ${singleRace.name}</p>`);
+        $(".userResultsContainer").append(`<p class="apiRace"><span class="resultSpan">Race: </span> ${singleRace.race}</p>`);
        
     } 
 )};
 
-// CREATE FUNCTION FOR ON SUBMIT BY USER
 weedApp.submitClick = function () {
-    $('form').submit(function (event) {
+    $('.moodForm').submit(function (event) {
         event.preventDefault();
         console.log('clicked!');
 
-        const strain = $("input[name=strain]:checked").val();
-        
-        animatedResults();
+        const race = $("input[name=race]:checked").val();
 
         $('html, body').animate({
-            scrollTop: $('.formContainer').offset().top + $('.formContainer').height()
+            scrollTop: $('.moodFormContainer').offset().top + $('.moodFormContainer').height()
         }, 400);
 
-        if ($("input[name=strain]:checked").length) {
+        if ($("input[name=race]:checked").length) {
             $('input[type=submit]', this).attr('disabled', 'disabled');
-        } else if (!$("input[name=strain]:checked").length) {
-            alert(`Are you already high? Must choose one option before submitting!`);
+            animatedResults();
+        } else if (!$("input[name=race]:checked").length) {
+            alert(`Are you already HIGH?? Please select one option before submitting!`);
+            $('input[type=submit]', this).attr('', '');
         };
 
-        weedApp.getStrain(strain);
+        weedApp.getRace(race);
 
     });
 };
 
 animatedResults = function(){
-    $('.resultContainer').css('opacity', '1').css('visibility', 'visible');
+    $('.resultContainer').css('opacity', '1').css('display', 'flex');
 }
 
 $(document).ready(function(){
